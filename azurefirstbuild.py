5ffbae148c9e06abf46037a5a34815f192253cce
Birudaraju <sreekanth.birudaraju@capgemini.com>
2019-07-31 16:13:44 +0530

    My forst Azure Jenkins Code

---
+++ azurefirstbuild.py
@@ -0,0 +1,1 @@
+print ("this is my first azure jenkins python code")
--- samplecode.py
+++
@@ -1,3 +0,0 @@
-print ("Hello from zure_Jenkins_Freestyle_Build1")
-print ("Hello from Azure Jenkins VM on Git")
-print ("Hello from Python on Git")
